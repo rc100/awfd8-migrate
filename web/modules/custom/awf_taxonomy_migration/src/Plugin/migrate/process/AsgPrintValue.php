e69a585953cf4abf8a0e8e30d3f2a726a01e4a7c
Sean Gleason <sean.gleason@Sean-Gleasons-MacBook-Pro.local>
2019-11-29 11:34:18 -0500

    paragraphs adding inital migration for paragraphs

---
+++ web/modules/custom/awf_taxonomy_migration/src/Plugin/migrate/process/AsgPrintValue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\awf_taxonomy_migration\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * @MigrateProcessPlugin(
+ *	id = "asg_print"
+ * 
+ * )
+**/
+
+class AsgPrintValue extends ProcessPluginBase {
+
+	public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property){
+
+		// print_r('asg ' . $value);
+
+		return $value;
+
+	}
+
+}
---
+++ web/modules/custom/awf_taxonomy_migration/src/Plugin/migrate/source/Node.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\awf_taxonomy_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\node\Plugin\migrate\source\d7\Node as D7Node;
+
+
+/**
+ * 
+ * @MigrateSource(
+ * 		id = "custom_node_source",
+ * 		source_module = "node"
+ * )
+ * 
+ */
+
+
+class Node extends D7Node {
+
+	public function fields(){
+		return ['alias' => $this->t('Path alias')] + parent::fields();
+	}
+
+
+	public function prepareRow(Row $row) {
+		$nid = $row->getSourceProperty('nid');
+		$query = $this->select('url_alias','ua')->fields('ua', ['alias']);
+		$query->condition('ua.source', 'node/'  . $nid);
+		$alias = $query->execute()->fetchfield();
+		if(!empty($alias)) {
+			$row->setSourceProperty('alias', '/' . $alias);
+		} else {
+			$row->setSourceProperty('alias', '/unknown');
+		}
+
+		return parent::prepareRow($row);
+
+	}
+
+
+}
