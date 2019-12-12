cfb32acb2dbbb18a4cf46027135052898afd9ce8
Sean Gleason <sean.gleason@Sean-Gleasons-MacBook-Pro.local>
2019-12-12 14:22:45 -0500

    updating based on secrets config

--- web/sites/default/settings.migrate-on-pantheon.php
+++ web/sites/default/settings.migrate-on-pantheon.php
@@ -6,33 +6,26 @@
 
 	if( isset($_ENV['PANTHEON_ENVIRONMENT']) ){
 
-
-//  typically this is /var/www
-		print( 'asg ' . $_SERVER['HOME'] . '/files/private/secrets.json' );
 		$secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
 		if(file_exists($secretsFile)) {
-			$secrets = json_decode(file_get_contents($secretsFile), 1);
+			$secrets = json_decode(file_get_contents($secretsFile), TRUE);
 		}
 
-		if(!empty($secrets['migrate_source_db__url'])){
-			$parsed_url = parse_url($secrets['migrate_source_db__url']);
-
-			if( !empty($parsed_url['migrate_source_db__database']) && 
-				!empty($parsed_url['migrate_source_db__username']) && 
-				!empty($parsed_url['migrate_source_db__pass']) && 
-				!empty($parsed_url['migrate_source_db__host']) && 
-				!empty($parsed_url['migrate_source_db__port']) ) {
-				$databases['migrate']['default'] = [
-				  'database' => $parsed_url['migrate_source_db__database'],
-				  'username' => $parsed_url['migrate_source_db__username'],
-				  'password' => $parsed_url['migrate_source_db__pass'],
-				  'host' => $parsed_url['migrate_source_db__host'],
-				  'port' => $parsed_url['migrate_source_db__port'],
-				  'driver' => 'mysql',
-				  'prefix' => '',
-				  'collation' => 'utf8mb4_general_ci',
-				];
-			}
+		if( !empty($secrets['migrate_source_db__database']) && 
+			!empty($secrets['migrate_source_db__username']) && 
+			!empty($secrets['migrate_source_db__pass']) && 
+			!empty($secrets['migrate_source_db__host']) && 
+			!empty($secrets['migrate_source_db__port']) ) {
+			$databases['migrate']['default'] = [
+			  'database' => $secrets['migrate_source_db__database'],
+			  'username' => $secrets['migrate_source_db__username'],
+			  'password' => $secrets['migrate_source_db__pass'],
+			  'host' => $secrets['migrate_source_db__host'],
+			  'port' => $secrets['migrate_source_db__port'],
+			  'driver' => 'mysql',
+			  'prefix' => '',
+			  'collation' => 'utf8mb4_general_ci',
+			];
 		}
 
 	} else {
