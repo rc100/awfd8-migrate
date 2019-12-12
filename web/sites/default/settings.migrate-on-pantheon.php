a5c938d4ce19e504885f6c616420848d2b180ba8
Sean Gleason <sean.gleason@Sean-Gleasons-MacBook-Pro.local>
2019-12-12 11:03:29 -0500

    updating connection info for terminus secrets

--- web/sites/default/settings.migrate-on-pantheon.php
+++ web/sites/default/settings.migrate-on-pantheon.php
@@ -1,40 +1,39 @@
 <?php
 
 
-if(isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+	if(isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+
+
+	} else {
+		
+	}
+
 
 	$secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
+
+
 	if(file_exists($secretsFile)) {
 		$secrets = json_decode(file_get_contents($secretsFile), 1);
 	}
 
 	if(!empty($secrets['migrate_source_db__url'])){
-	$parsed_url = parse_url($secrets['migrate_source_db__url']);
+		$parsed_url = parse_url($secrets['migrate_source_db__url']);
 
-		if(!empty($parsed_url['port']) && !empty($parsed_url['host']) && !empty($parsed_url['pass'])) {
-			$databases['migrate']['default'] = [
-			  'database' => 'pantheon',
-			  'username' => 'pantheon',
-			  'password' => $parsed_url['pass'],
-			  'host' => $parsed_url['host'],
-			  'port' => $parsed_url['port'],
-			  'driver' => 'mysql',
-			  'prefix' => '',
-			  'collation' => 'utf8mb4_general_ci',
-			];
-		}
+	if( !empty($parsed_url['database']) && 
+		!empty($parsed_url['username']) && 
+		!empty($parsed_url['pass']) && 
+		!empty($parsed_url['host']) && 
+		!empty($parsed_url['port']) && 
+		) {
+		$databases['migrate']['default'] = [
+		  'database' => 'pantheon',
+		  'username' => 'pantheon',
+		  'password' => $parsed_url['pass'],
+		  'host' => $parsed_url['host'],
+		  'port' => $parsed_url['port'],
+		  'driver' => 'mysql',
+		  'prefix' => '',
+		  'collation' => 'utf8mb4_general_ci',
+		];
 	}
-
-} else {
-	$databases['migrate']['default'] = [
-	  'database' => 'database',
-	  'username' => 'mysql',
-	  'password' => 'mysql',
-	  'host' => 'legacy',
-	  'port' => '3306',
-	  'driver' => 'mysql',
-	  'prefix' => '',
-	  'collation' => 'utf8mb4_general_ci',
-	];
-}
-
+	}
