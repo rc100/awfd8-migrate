de9602610773b1571ab11407f7ee7d0e1614b148
Sean Gleason <sean.gleason@Sean-Gleasons-MacBook-Pro.local>
2019-12-12 12:05:53 -0500

    adjusting second database connection.

--- web/sites/default/settings.migrate-on-pantheon.php
+++ web/sites/default/settings.migrate-on-pantheon.php
@@ -1,39 +1,40 @@
 <?php
 
+	isset($_ENV['PANTHEON_ENVIRONMENT']) ? $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json' : 
+	$_SERVER['HOME'] . '/lando_config/secret/secrets.json';
 
-	if(isset($_ENV['PANTHEON_ENVIRONMENT'])) {
 
+	if( isset($_ENV['PANTHEON_ENVIRONMENT']) ){
+
+		$secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
+		if(file_exists($secretsFile)) {
+			$secrets = json_decode(file_get_contents($secretsFile), 1);
+		}
+
+		if(!empty($secrets['migrate_source_db__url'])){
+			$parsed_url = parse_url($secrets['migrate_source_db__url']);
+
+			if( !empty($parsed_url['migrate_source_db__database']) && 
+				!empty($parsed_url['migrate_source_db__username']) && 
+				!empty($parsed_url['migrate_source_db__pass']) && 
+				!empty($parsed_url['migrate_source_db__host']) && 
+				!empty($parsed_url['migrate_source_db__port']) && ) {
+				$databases['migrate']['default'] = [
+				  'database' => $parsed_url['migrate_source_db__database'],
+				  'username' => $parsed_url['migrate_source_db__username'],
+				  'password' => $parsed_url['migrate_source_db__pass'],
+				  'host' => $parsed_url['migrate_source_db__host'],
+				  'port' => $parsed_url['migrate_source_db__port'],
+				  'driver' => 'mysql',
+				  'prefix' => '',
+				  'collation' => 'utf8mb4_general_ci',
+				];
+			}
+		}
 
 	} else {
-		
-	}
-
-
-	$secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
-
-
-	if(file_exists($secretsFile)) {
-		$secrets = json_decode(file_get_contents($secretsFile), 1);
-	}
-
-	if(!empty($secrets['migrate_source_db__url'])){
-		$parsed_url = parse_url($secrets['migrate_source_db__url']);
-
-	if( !empty($parsed_url['database']) && 
-		!empty($parsed_url['username']) && 
-		!empty($parsed_url['pass']) && 
-		!empty($parsed_url['host']) && 
-		!empty($parsed_url['port']) && 
-		) {
-		$databases['migrate']['default'] = [
-		  'database' => 'pantheon',
-		  'username' => 'pantheon',
-		  'password' => $parsed_url['pass'],
-		  'host' => $parsed_url['host'],
-		  'port' => $parsed_url['port'],
-		  'driver' => 'mysql',
-		  'prefix' => '',
-		  'collation' => 'utf8mb4_general_ci',
-		];
-	}
+		$secetsFile = $_SERVER['HOME'] . '/lando_config/secret/secrets.json';
+		if(file_exists($secetsFile)) {
+			
+		}
 	}
