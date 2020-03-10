f979cc4feb38999b030bf989e37096779aa199e0
Blake Scott <blakerscott@gmail.com>
2020-03-10 12:15:19 -0700

    awf-394 - js functionality to show hidden annual reports when button is clicked

--- web/themes/custom/awf/src/js/includes/show-more.js
+++ web/themes/custom/awf/src/js/includes/show-more.js
@@ -1,12 +1,13 @@
 /**
  * @file
- * Custom display code for the /about/history page.
+ * Custom display code for the /about/history and /about/resources pages.
  *
  */
 
 (function ($) {
   // DOC READY
   $(function () {
+    //START CODE for about/history page.
     //Add a class to all Years and Descriptions after the first two.
     $('.field--name-field-year-and-description').each(function(){
       $(this).find('.field__item').not( ':nth-child(1)' ).not( ':nth-child(2)' ).addClass('field__item_hidden');
@@ -15,7 +16,6 @@
     $( '.field__item_hidden' ).hide();
     //Hide the Show Me More element if a Decade has less than 3 Years.
     $('.paragraph--type--history-list').each(function(){
-      var $this = $(this);
       if($(this).find('.field__item_hidden').length == 0) {
         $(this).find( '.show-more-button' ).hide();
       }
@@ -25,5 +25,14 @@
       $( this ).hide();
       $( this ).parents('.paragraph--type--history-list').find('.field__item_hidden').show(900);
     });      
+    //END CODE for /about/history page, start code for /about/resources page.
+    
+    //Hide the relevant divs.
+    $('.view-display-id-block_2').find('.orange.carrot').hide();
+    //Show relevant divs, hide archive button.
+    $('.archive-button').on('click', function(e) {
+      $( this ).hide();
+      $('.view-display-id-block_2').find('.orange.carrot').show(900);
+    });   
   }); 
 })(jQuery);
